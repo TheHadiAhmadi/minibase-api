da2f9634957ad9ecc962202d4faebac38547876f
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-10-24 17:21:46 +0430

    support OPTIONS method

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,16 @@
 export async function handle({ event, resolve }) {
+  if (event.request.method === "OPTIONS") {
+    const response = new Response(undefined, {
+      status: 200,
+    });
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "*");
+    response.headers.set("Access-Control-Allow-Headers", "Authorization,x-*");
+    response.headers.set("Cross-Origin-Resource-Policy", "cross-origin");
+
+    return response;
+  }
+
   const url = new URL(event.request.url);
 
   let project = null;
