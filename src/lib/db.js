d605f077dc037403abaed332f2afdc4c489017b9
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-10-31 18:01:14 +0430

    use sqlite

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,10 @@
-import { DATABASE_URI } from "$env/static/private";
+import { DATABASE_FILE } from "$env/static/private";
 import knex from "knex";
-
 
 const db = knex({
   useNullAsDefault: true,
-  client: "mysql",
-  connection: DATABASE_URI,
+  client: "sqlite3",
+  connection: DATABASE_FILE,
 });
 
 export default db;
