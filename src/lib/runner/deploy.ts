f1197120aa196b701ddf36ce0519d830903f1128
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-12-11 02:04:21 +0430

    enable cors

--- src/lib/runner/deploy.ts
+++ src/lib/runner/deploy.ts
@@ -65,6 +65,24 @@ async function createDeployment({
           dest: "index.js",
         },
       ],
+      headers: [
+        {
+          source: "/(.*)",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value:
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ],
       projectSettings: {
         framework: null,
       },
